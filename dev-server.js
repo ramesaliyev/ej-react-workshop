1b52c1d4db2c9262085bfe588552ffaf4ea0d9db
Rameş Aliyev <rames.aliyev@emlakjet.com>
2017-05-05 08:21:49 +0300

    Add webpack dev server.

--- dev-server.js
+++ dev-server.js
@@ -1,10 +1,19 @@
 const path = require('path');
 const Express = require('express');
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpack = require('webpack');
+const config = require('./webpack.config');
 
 const app = new Express();
 const port = 3000;
 
 app.use(Express.static('dist'))
+
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: '/',
+}));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
