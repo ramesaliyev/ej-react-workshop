dcbd9e26491dba9451f985be53e25030b8cf771c
Rameş Aliyev <rames.aliyev@emlakjet.com>
2017-05-05 14:05:36 +0300

    Convert functional component to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './App.css';
 
-const App = () => (
-  <div className={styles.wrapper}>
-    <div className={styles.addWrapper}>
-      <input className={styles.input} />
-      <button className={styles.button} >Add</button>
-    </div>
-    <div className={styles.listWrapper} >
-      <ul className={styles.list}></ul>    
-    </div>
-  </div>
-);
+class App extends Component {
+  render () {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.addWrapper}>
+          <input className={styles.input} />
+          <button className={styles.button} >Add</button>
+        </div>
+        <div className={styles.listWrapper} >
+          <ul className={styles.list}></ul>    
+        </div>
+      </div>  
+    );
+  }
+}
 
 export default App;
