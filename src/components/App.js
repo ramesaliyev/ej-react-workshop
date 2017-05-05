9b242bed9969b7f9deb85a5fc5c0ad5373bfc4fd
Rameş Aliyev <rames.aliyev@emlakjet.com>
2017-05-05 09:25:35 +0300

    Create react components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <div>
+      <input /> <button>Add</button>
+    </div>
+    <div>
+      <h1>Todo List</h1>
+      <ul></ul>    
+    </div>
+  </div>
+);
+
+export default App;
