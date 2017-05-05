bd9315b9b053538dd4082dd2e6dc79fe206c9d13
Rameş Aliyev <rames.aliyev@emlakjet.com>
2017-05-05 14:48:43 +0300

    Use react state

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import styles from './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      list: [ 'kamil', 'fadime' ]
+    }
+  }
+
   render () {
     return (
       <div className={styles.wrapper}>
         <div className={styles.addWrapper}>
           <input className={styles.input} />
-          <button className={styles.button} >Add</button>
+          <button
+            className={styles.button}
+            onClick={() => this.setState({
+              list: [
+                ...this.state.list,
+                Date.now()
+              ]
+            })}
+          >Add</button>
         </div>
         <div className={styles.listWrapper} >
-          <ul className={styles.list}></ul>    
+          <ul className={styles.list}>
+            {this.state.list.map(item => 
+              <li key={item}>{item}</li>
+            )}
+          </ul>    
         </div>
       </div>  
     );
